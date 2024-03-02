046d56e9372d1577dbefd78dc7a77ca92ea8158d
Paul Deen <paul.deen@dutchworkz.nl>
2024-03-02 19:52:22 +0100

    Attempt to trick Sonar for code duplication check

--- src/ClassFramework.CodeGeneration.Bootstrap/Models/TemplateFramework/ICsharpClassGeneratorSettings.cs
+++ src/ClassFramework.CodeGeneration.Bootstrap/Models/TemplateFramework/ICsharpClassGeneratorSettings.cs
@@ -3,16 +3,22 @@
 internal interface ICsharpClassGeneratorSettings
 {
     bool RecurseOnDeleteGeneratedFiles { get; }
-    [Required(AllowEmptyStrings = true)] string LastGeneratedFilesFilename { get; }
-    [Required] Encoding Encoding { get; }
+    [Required(AllowEmptyStrings = true)]
+    string LastGeneratedFilesFilename { get; }
+    [Required]
+    Encoding Encoding { get; }
 
-    [Required(AllowEmptyStrings = true)] string Path { get; }
-    [Required] CultureInfo CultureInfo { get; }
+    [Required(AllowEmptyStrings = true)]
+    string Path { get; }
+    [Required]
+    CultureInfo CultureInfo { get; }
     bool GenerateMultipleFiles { get; }
     bool SkipWhenFileExists { get; }
     bool CreateCodeGenerationHeader { get; }
-    [Required(AllowEmptyStrings = true)] string EnvironmentVersion { get; }
-    [Required(AllowEmptyStrings = true)] string FilenameSuffix { get; }
+    [Required(AllowEmptyStrings = true)]
+    string EnvironmentVersion { get; }
+    [Required(AllowEmptyStrings = true)]
+    string FilenameSuffix { get; }
     bool EnableNullableContext { get; }
     bool EnableGlobalUsings { get; }
 }
