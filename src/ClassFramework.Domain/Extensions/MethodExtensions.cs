41205cf41abc4c9701c6c8b13c8994a6be342921
Paul Deen <paul.deen@dutchworkz.nl>
2024-03-02 11:45:20 +0100

    Temporarily refactored code into extension method, so we can switch between record an class (old and new code generation)

---
+++ src/ClassFramework.Domain/Extensions/MethodExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace ClassFramework.Domain;
+
+public static class MethodExtensions
+{
+    public static bool IsInterfaceMethod(this Method instance)
+        => instance.Name.StartsWith("I", StringComparison.Ordinal)
+        && instance.Name.Length > 1
+        && instance.Name.Contains('.');
+}
