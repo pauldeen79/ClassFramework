56fe5ceef466c85d7a052a535d74e783b9cf73e3
Paul Deen <paul.deen@dutchworkz.nl>
2024-03-17 17:01:35 +0100

    Put back PropertyBuilder as a legacy thingy. Needed in case you need to have full compatibility with the bootstrap generated code...

---
+++ src/ClassFramework.Domain/Builders/PropertyBuilder.cs
@@ -0,0 +1,11 @@
+﻿namespace ClassFramework.Domain.Builders;
+
+public partial class PropertyBuilder
+{
+    // uncomment the following code, if you need default values and you are using the Bootstrap generated code (or you have disabled the DefaultAttribute value copying to the builder constructor)
+    ///partial void SetDefaultValues()
+    ///{
+    ///    HasGetter = true;
+    ///    HasSetter = true;
+    ///}
+}
