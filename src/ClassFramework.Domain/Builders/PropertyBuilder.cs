4c35728b345a4f0d7c5ddd09d28032390966a86f
Paul Deen <paul.deen@dutchworkz.nl>
2024-03-17 17:10:04 +0100

    Put back commented code. Don't know why, but it seems necessary...

--- src/ClassFramework.Domain/Builders/PropertyBuilder.cs
+++ src/ClassFramework.Domain/Builders/PropertyBuilder.cs
@@ -2,10 +2,9 @@
 
 public partial class PropertyBuilder
 {
-    // uncomment the following code, if you need default values and you are using the Bootstrap generated code (or you have disabled the DefaultAttribute value copying to the builder constructor)
-    ///partial void SetDefaultValues()
-    ///{
-    ///    HasGetter = true;
-    ///    HasSetter = true;
-    ///}
+    partial void SetDefaultValues()
+    {
+        HasGetter = true;
+        HasSetter = true;
+    }
 }
