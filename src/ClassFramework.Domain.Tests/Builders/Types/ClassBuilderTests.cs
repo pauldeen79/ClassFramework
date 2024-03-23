7761ef349b28c9e6d070f8d33e5449f410e47fe8
Paul Deen <paul.deen@dutchworkz.nl>
2024-03-23 07:46:40 +0100

    Added test for shared validation

--- src/ClassFramework.Domain.Tests/Builders/Types/ClassBuilderTests.cs
+++ src/ClassFramework.Domain.Tests/Builders/Types/ClassBuilderTests.cs
@@ -3,17 +3,32 @@
 public class ClassBuilderTests : TestBase<ClassBuilder>
 {
     [Fact]
-    public void Can_Validate_Recursively()
+    public void Can_Validate_Recursively_Using_TryValidate()
     {
         // Arrange
         var sut = CreateSut().AddFields(new FieldBuilder());
+        var validationResults = new List<ValidationResult>();
 
         // Act
-        var validationResults = new List<ValidationResult>();
         var success = sut.TryValidate(validationResults);
 
         // Assert
         success.Should().BeFalse();
         validationResults.Should().HaveCount(2); //both the validation errors in Class and Field
+    }
+
+    [Fact]
+    public void Can_Validate_Recursively_Using_Validate()
+    {
+        // Arrange
+        var sut = CreateSut().AddFields(new FieldBuilder());
+        var validationContext = new ValidationContext(sut);
+        var validatableObject = sut as IValidatableObject; // can only do this with shared validation... else, the builder would not implement IValidatableObject by default
+
+        // Act & Assert
+        if (validatableObject is not null)
+        {
+            validatableObject.Invoking(x => x.Validate(validationContext)).Should().Throw<ValidationException>();
+        }
     }
 }
