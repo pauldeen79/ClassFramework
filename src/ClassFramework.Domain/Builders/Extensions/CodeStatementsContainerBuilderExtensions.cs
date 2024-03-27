a5aa19c157bef838a4bd05a3993096e508b3dcce
Paul Deen <paul.deen@dutchworkz.nl>
2024-03-27 11:49:34 +0100

    Added NotImplemented methods to code statement container and property builders

--- src/ClassFramework.Domain/Builders/Extensions/CodeStatementsContainerBuilderExtensions.cs
+++ src/ClassFramework.Domain/Builders/Extensions/CodeStatementsContainerBuilderExtensions.cs
@@ -3,13 +3,11 @@
 public static partial class CodeStatementsContainerBuilderExtensions
 {
     public static T AddStringCodeStatements<T>(this T instance, params string[] statements) where T : ICodeStatementsContainerBuilder
-    {
-        instance.CodeStatements.AddRange(statements.IsNotNull(nameof(statements)).Select(x => new StringCodeStatementBuilder().WithStatement(x)));
-        return instance;
-    }
+        => instance.AddCodeStatements(statements.IsNotNull(nameof(statements)).Select(x => new StringCodeStatementBuilder().WithStatement(x)));
 
     public static T AddStringCodeStatements<T>(this T instance, IEnumerable<string> statements) where T : ICodeStatementsContainerBuilder
-    {
-        return instance.AddStringCodeStatements(statements.IsNotNull(nameof(statements)).ToArray());
-    }
+        => instance.AddStringCodeStatements(statements.IsNotNull(nameof(statements)).ToArray());
+
+    public static T NotImplemented<T>(this T instance) where T : ICodeStatementsContainerBuilder
+        => instance.AddStringCodeStatements(WellKnownCodeStatements.ThrowNotImplementedException);
 }
---
+++ src/ClassFramework.Domain/Domains/WellKnownCodeStatements.cs
@@ -0,0 +1,6 @@
+﻿namespace ClassFramework.Domain.Domains;
+
+public static class WellKnownCodeStatements
+{
+    public static readonly string ThrowNotImplementedException = $"throw new {typeof(NotImplementedException).FullName}();";
+}
