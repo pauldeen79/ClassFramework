ff8f1693245a6bbc670495398901089b5d90a3ea
Paul Deen <paul.deen@dutchworkz.nl>
2024-04-27 20:21:42 +0200

    Revert "Working... Quirking... not happy :("
    This reverts commit 512f81e4d777c9c3fd0baf50edec9329988eae35.

--- src/ClassFramework.TemplateFramework/CodeGenerationProviders/CsharpClassGeneratorPipelineCodeGenerationProviderBase.cs
+++ src/ClassFramework.TemplateFramework/CodeGenerationProviders/CsharpClassGeneratorPipelineCodeGenerationProviderBase.cs
@@ -1,6 +1,4 @@
-﻿using ClassFramework.Domain.Builders.Extensions;
-
-namespace ClassFramework.TemplateFramework.CodeGenerationProviders;
+﻿namespace ClassFramework.TemplateFramework.CodeGenerationProviders;
 
 public abstract class CsharpClassGeneratorPipelineCodeGenerationProviderBase : CsharpClassGeneratorCodeGenerationProviderBase
 {
@@ -187,7 +185,7 @@ public abstract class CsharpClassGeneratorPipelineCodeGenerationProviderBase : C
         Guard.IsNotNull(interfacesNamespace);
 
         return (await (await GetBuilders(models, buildersNamespace, entitiesNamespace))
-                .SelectAsync(async x => (await CreateInterface(x, interfacesNamespace, BuilderCollectionType.WithoutGenerics(), true, "I{Class.Name}", (t, m) => InheritFromInterfaces && m.Name == BuildMethodName && t.Interfaces.Count == 0 && string.IsNullOrEmpty(m.ExplicitInterfaceName))).ToBuilder().AddMethods(new MethodBuilder().WithName("Build").WithReturnTypeName($"I{x.Name.ReplaceSuffix("Builder", string.Empty, StringComparison.Ordinal)}")).Build())
+                .SelectAsync(async x => await CreateInterface(x, interfacesNamespace, BuilderCollectionType.WithoutGenerics(), true, "I{Class.Name}", (t, m) => InheritFromInterfaces && m.Name == BuildMethodName && t.Interfaces.Count == 0 && string.IsNullOrEmpty(m.ExplicitInterfaceName)))
                ).ToArray();
     }
 
