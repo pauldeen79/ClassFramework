f5669854b809af7a2bf4a332c9fab84a4b0c4c27
Paul Deen <paul.deen@dutchworkz.nl>
2024-04-27 20:10:00 +0200

    First steps into generating explicit Build methods on builders

--- src/ClassFramework.Pipelines/Builder/Components/AddInterfacesComponent.cs
+++ src/ClassFramework.Pipelines/Builder/Components/AddInterfacesComponent.cs
@@ -64,6 +64,25 @@ public class AddInterfacesComponent : IPipelineComponent<BuilderContext, IConcre
 
         context.Response.AddInterfaces(results.Select(x => x.Value!));
 
+        var explicitInterfaceEntities = context.Request.SourceModel.Interfaces
+            .Where(x => context.Request.Settings.CopyInterfacePredicate?.Invoke(x) ?? true)
+            .Select(x => context.Request.MapTypeName(x.FixTypeName(), MetadataNames.CustomEntityInterfaceTypeName))
+            .ToArray();
+
+        var methodName = string.IsNullOrEmpty(context.Request.SourceModel.GetEntityBaseClass(context.Request.Settings.EnableInheritance, context.Request.Settings.BaseClass))
+            ? context.Request.Settings.BuildMethodName
+            : context.Request.Settings.BuildTypedMethodName;
+
+        foreach (var item in results.Zip(explicitInterfaceEntities, (builder, entity) => new { Builder = builder, Entity = entity }).Where(x => x.Builder.Value!.EndsWith("Builder") && x.Builder.Value!.GetClassName() != $"I{context.Request.SourceModel.Name}Builder"))
+        {
+            context.Response.AddMethods(new MethodBuilder()
+                .WithName(methodName)
+                .WithExplicitInterfaceName(item.Builder.Value!)
+                .WithReturnTypeName(item.Entity)
+                .AddStringCodeStatements($"return {methodName}();")
+                );
+        }
+
         return Task.FromResult(Result.Continue());
     }
 }
