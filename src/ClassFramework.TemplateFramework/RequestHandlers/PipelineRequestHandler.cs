b83ae80e6ef9a5ae5075f894c417d109cc17d1a1
Paul Deen <paul.deen@dutchworkz.nl>
2024-04-30 13:04:58 +0200

    Refactored code to remove code duplication

--- src/ClassFramework.TemplateFramework/RequestHandlers/PipelineRequestHandler.cs
+++ src/ClassFramework.TemplateFramework/RequestHandlers/PipelineRequestHandler.cs
@@ -1,11 +1,10 @@
 ﻿namespace ClassFramework.TemplateFramework.RequestHandlers;
 
-public class ClassFrameworkPipelineRequestHandler<TResponse, TContext> : IRequestHandler<PipelineRequest<TContext, TResponse>, Result<TResponse>>
-    where TContext : ContextBase<TypeBase, TResponse>
+public abstract class PipelineRequestHandler<TResponse, TContext> : IRequestHandler<PipelineRequest<TContext, TResponse>, Result<TResponse>>
 {
     private readonly IPipeline<TContext> _pipeline;
 
-    public ClassFrameworkPipelineRequestHandler(IPipeline<TContext> pipeline)
+    protected PipelineRequestHandler(IPipeline<TContext> pipeline)
     {
         Guard.IsNotNull(pipeline);
 
@@ -24,45 +23,40 @@ public class ClassFrameworkPipelineRequestHandler<TResponse, TContext> : IReques
         }
 
         var validationResults = new List<ValidationResult>();
-        var success = request.Context.ResponseBuilder.TryValidate(validationResults);
+        var responseBuilder = GetResponseBuilder(request.Context);
+        var success = responseBuilder.TryValidate(validationResults);
         if (!success)
         {
             return Result.Invalid<TResponse>("Pipeline response is not valid", validationResults.Select(x => new ValidationError(x.ErrorMessage ?? string.Empty, x.MemberNames)));
         }
 
-        return Result.FromExistingResult(result, request.Context.ResponseBuilder.Build());
+        return Result.FromExistingResult(result, BuildResponseValue(request.Context));
     }
+
+    protected abstract IBuilder<TResponse> GetResponseBuilder(TContext context);
+    protected abstract TResponse BuildResponseValue(TContext context);
 }
 
-public class ReflectionPipelineRequestHandler<TResponse, TContext> : IRequestHandler<PipelineRequest<TContext, TResponse>, Result<TResponse>>
+public class ClassFrameworkPipelineRequestHandler<TResponse, TContext> : PipelineRequestHandler<TResponse, TContext>
+    where TContext : ContextBase<TypeBase, TResponse>
+{
+    public ClassFrameworkPipelineRequestHandler(IPipeline<TContext> pipeline) : base(pipeline)
+    {
+    }
+
+    protected override TResponse BuildResponseValue(TContext context) => context.IsNotNull(nameof(context)).ResponseBuilder.Build();
+
+    protected override IBuilder<TResponse> GetResponseBuilder(TContext context) => context.IsNotNull(nameof(context)).ResponseBuilder;
+}
+
+public class ReflectionPipelineRequestHandler<TResponse, TContext> : PipelineRequestHandler<TResponse, TContext>
     where TContext : ContextBase<Type, TResponse>
 {
-    private readonly IPipeline<TContext> _pipeline;
-
-    public ReflectionPipelineRequestHandler(IPipeline<TContext> pipeline)
+    public ReflectionPipelineRequestHandler(IPipeline<TContext> pipeline) : base(pipeline)
     {
-        Guard.IsNotNull(pipeline);
-
-        _pipeline = pipeline;
     }
 
-    public async Task<Result<TResponse>> Handle(PipelineRequest<TContext, TResponse> request, CancellationToken cancellationToken)
-    {
-        Guard.IsNotNull(request);
+    protected override TResponse BuildResponseValue(TContext context) => context.IsNotNull(nameof(context)).ResponseBuilder.Build();
 
-        var result = await _pipeline.Process(request.Context, cancellationToken);
-        if (!result.IsSuccessful())
-        {
-            return Result.FromExistingResult<TResponse>(result);
-        }
-
-        var validationResults = new List<ValidationResult>();
-        var success = request.Context.ResponseBuilder.TryValidate(validationResults);
-        if (!success)
-        {
-            return Result.Invalid<TResponse>("Pipeline response is not valid", validationResults.Select(x => new ValidationError(x.ErrorMessage ?? string.Empty, x.MemberNames)));
-        }
-
-        return Result.FromExistingResult(result, request.Context.ResponseBuilder.Build());
-    }
+    protected override IBuilder<TResponse> GetResponseBuilder(TContext context) => context.IsNotNull(nameof(context)).ResponseBuilder;
 }
