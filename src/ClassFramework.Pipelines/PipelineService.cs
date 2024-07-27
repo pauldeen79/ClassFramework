c434fd4d4e7eb4a566318aa882ce8b829c2c16e6
Paul Deen <paul.deen@dutchworkz.nl>
2024-07-27 16:59:42 +0200

    Upgraded Pipelines package to latest version

--- src/ClassFramework.Pipelines/PipelineService.cs
+++ src/ClassFramework.Pipelines/PipelineService.cs
@@ -25,52 +25,35 @@ public class PipelineService : IPipelineService
     public async Task<Result<TypeBase>> Process(BuilderExtensionContext context, CancellationToken cancellationToken)
     {
         context = context.IsNotNull(nameof(context));
-        var result = await _builderExtensionPipeline.Process(context, cancellationToken).ConfigureAwait(false);
-        return ProcessResult(result, context.Builder, context.Builder.Build);
+        return (await _builderExtensionPipeline.Process(context, cancellationToken).ConfigureAwait(false))
+            .ProcessResult(context.Builder, context.Builder.Build);
     }
 
     public async Task<Result<TypeBase>> Process(BuilderContext context, CancellationToken cancellationToken)
     {
         context = context.IsNotNull(nameof(context));
-        var result = await _builderPipeline.Process(context, cancellationToken).ConfigureAwait(false);
-        return ProcessResult(result, context.Builder, context.Builder.Build);
+        return (await _builderPipeline.Process(context, cancellationToken).ConfigureAwait(false))
+            .ProcessResult(context.Builder, context.Builder.Build);
     }
 
     public async Task<Result<TypeBase>> Process(EntityContext context, CancellationToken cancellationToken)
     {
         context = context.IsNotNull(nameof(context));
-        var result = await _entityPipeline.Process(context, cancellationToken).ConfigureAwait(false);
-        return ProcessResult(result, context.Builder, context.Builder.Build);
+        return (await _entityPipeline.Process(context, cancellationToken).ConfigureAwait(false))
+            .ProcessResult(context.Builder, context.Builder.Build);
     }
 
     public async Task<Result<Domain.Types.Interface>> Process(InterfaceContext context, CancellationToken cancellationToken)
     {
         context = context.IsNotNull(nameof(context));
-        var result = await _interfacePipeline.Process(context, cancellationToken).ConfigureAwait(false);
-        return ProcessResult(result, context.Builder, context.Builder.BuildTyped);
+        return (await _interfacePipeline.Process(context, cancellationToken).ConfigureAwait(false))
+            .ProcessResult(context.Builder, context.Builder.BuildTyped);
     }
 
     public async Task<Result<TypeBase>> Process(Reflection.ReflectionContext context, CancellationToken cancellationToken)
     {
         context = context.IsNotNull(nameof(context));
-        var result = await _reflectionPipeline.Process(context, cancellationToken).ConfigureAwait(false);
-        return ProcessResult(result, context.Builder, context.Builder.Build);
-    }
-
-    private static Result<TResult> ProcessResult<TResult>(Result result, object responseBuilder, Func<TResult> dlg)
-    {
-        if (!result.IsSuccessful())
-        {
-            return Result.FromExistingResult<TResult>(result);
-        }
-
-        var validationResults = new List<ValidationResult>();
-        var success = responseBuilder.TryValidate(validationResults);
-        if (!success)
-        {
-            return Result.Invalid<TResult>("Pipeline response is not valid", validationResults.Select(x => new ValidationError(x.ErrorMessage ?? string.Empty, x.MemberNames)));
-        }
-
-        return Result.FromExistingResult(result, dlg.Invoke());
+        return (await _reflectionPipeline.Process(context, cancellationToken).ConfigureAwait(false))
+            .ProcessResult(context.Builder, context.Builder.Build);
     }
 }
