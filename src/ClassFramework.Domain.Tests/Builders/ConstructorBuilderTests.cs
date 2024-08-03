2085b53c20a1ee37cf6d68c211c210ca9fdf2cfe
Paul Deen <paul.deen@dutchworkz.nl>
2024-08-03 09:34:37 +0200

    Added ChainCallToBaseUsingParameters method to ConstructorBuilder

---
+++ src/ClassFramework.Domain.Tests/Builders/ConstructorBuilderTests.cs
@@ -0,0 +1,23 @@
+﻿namespace ClassFramework.Domain.Tests.Builders;
+
+public class ConstructorBuilderTests : TestBase<ConstructorBuilder>
+{
+    public class ChainCallToBaseUsingParameters : ConstructorBuilderTests
+    {
+        [Fact]
+        public void Creates_ChainCall_Correctly()
+        {
+            // Arrange
+            var sut = CreateSut()
+                .AddParameter("param1", typeof(int))
+                .AddParameter("param2", typeof(string))
+                .AddParameter("param3", typeof(bool));
+
+            // Act
+            var actual = sut.ChainCallToBaseUsingParameters();
+
+            // Assert
+            actual.ChainCall.Should().Be("base(param1, param2, param3)");
+        }
+    }
+}
---
+++ src/ClassFramework.Domain/Builders/ConstructorBuilder.cs
@@ -0,0 +1,7 @@
+﻿namespace ClassFramework.Domain.Builders;
+
+public partial class ConstructorBuilder
+{
+    public ConstructorBuilder ChainCallToBaseUsingParameters()
+        => WithChainCall($"base({string.Join(", ", Parameters.Select(x => x.Name))})");
+}
