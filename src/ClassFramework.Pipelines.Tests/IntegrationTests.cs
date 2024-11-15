a362f996a28b3e5ba40b36ada8ff5900b78f2094
Paul Deen <paul.deen@dutchworkz.nl>
2024-11-15 13:39:25 +0100

    Added integration tests to prove we can use a formattable string parser instead of a format string parser

---
+++ src/ClassFramework.Pipelines.Tests/IntegrationTests.cs
@@ -0,0 +1,40 @@
+﻿namespace ClassFramework.Pipelines.Tests;
+
+public class IntegrationTests : IntegrationTestBase<IntegrationTests>
+{
+    [Fact]
+    public void Can_Use_FormattableStringParser_With_PlaceholderProcessors_To_Get_Formatted_String()
+    {
+        // Arrange
+        var formatString = "foreach (var item in {NameCamelCsharpFriendlyName}) {Name}.Add(item);";
+        var sut = Scope!.ServiceProvider.GetRequiredService<IFormattableStringParser>();
+        var property = new PropertyBuilder().WithName("MyProperty").WithType(typeof(string)).Build();
+        var pipelineSettings = new PipelineSettingsBuilder().Build();
+        var propertyContext = new PropertyContext(property, pipelineSettings, CultureInfo.InvariantCulture, "MyTypeName", typeof(List<string>).FullName!.WithoutProcessedGenerics());
+
+        // Act
+        var result = sut.Parse(formatString, CultureInfo.InvariantCulture, propertyContext);
+
+        // Assert
+        result.Status.Should().Be(ResultStatus.Ok);
+        result.GetValueOrThrow().ToString().Should().Be("foreach (var item in myProperty) MyProperty.Add(item);");
+    }
+
+    [Fact]
+    public void Can_Use_FormattableStringParser_With_Functions_To_Get_Formatted_String_And_Apply_Processing_Like_Casing()
+    {
+        // Arrange
+        var formatString = "foreach (var item in {ToCamelCase(PropertyName())}) {PropertyName()}.Add(item);";
+        var sut = Scope!.ServiceProvider.GetRequiredService<IFormattableStringParser>();
+        var property = new PropertyBuilder().WithName("MyProperty").WithType(typeof(string)).Build();
+        var pipelineSettings = new PipelineSettingsBuilder().Build();
+        var propertyContext = new PropertyContext(property, pipelineSettings, CultureInfo.InvariantCulture, "MyTypeName", typeof(List<string>).FullName!.WithoutProcessedGenerics());
+
+        // Act
+        var result = sut.Parse(formatString, CultureInfo.InvariantCulture, propertyContext);
+
+        // Assert
+        result.Status.Should().Be(ResultStatus.Ok);
+        result.GetValueOrThrow().ToString().Should().Be("foreach (var item in myProperty) MyProperty.Add(item);");
+    }
+}
