27ef940569e406cb5bca4b9723bca68b072c54c9
Paul Deen <paul.deen@dutchworkz.nl>
2024-12-13 20:17:57 +0100

    Minor: Renamed unit tets to follow the right pattern for function

--- src/ClassFramework.Pipelines.Tests/Shared/Functions/NullCheckFunctionTests.cs
+++ src/ClassFramework.Pipelines.Tests/Shared/Functions/NullCheckFunctionTests.cs
@@ -2,90 +2,93 @@
 
 public class NullCheckFunctionTests : TestBase<NullCheckFunction>
 {
-    [Fact]
-    public void Can_Get_NullCheck_From_ContextBase()
+    public class Parse : NullCheckFunctionTests
     {
-        // Arrange
-        InitializeParser();
-        var functionParseResult = new FunctionParseResultBuilder()
-            .WithFunctionName("NullCheck")
-            .WithFormatProvider(CultureInfo.InvariantCulture)
-            .Build();
-        var context = new PropertyContext(CreateProperty(), new PipelineSettingsBuilder().WithAddNullChecks().Build(), CultureInfo.InvariantCulture, typeof(string).FullName!, typeof(List<>).WithoutGenerics());
-        var evaluator = Fixture.Freeze<IFunctionParseResultEvaluator>();
-        var parser = Fixture.Freeze<IExpressionParser>();
-        var sut = CreateSut();
+        [Fact]
+        public void Returns_Success_On_Context_Of_Type_ContextBase()
+        {
+            // Arrange
+            InitializeParser();
+            var functionParseResult = new FunctionParseResultBuilder()
+                .WithFunctionName("NullCheck")
+                .WithFormatProvider(CultureInfo.InvariantCulture)
+                .Build();
+            var context = new PropertyContext(CreateProperty(), new PipelineSettingsBuilder().WithAddNullChecks().Build(), CultureInfo.InvariantCulture, typeof(string).FullName!, typeof(List<>).WithoutGenerics());
+            var evaluator = Fixture.Freeze<IFunctionParseResultEvaluator>();
+            var parser = Fixture.Freeze<IExpressionParser>();
+            var sut = CreateSut();
 
-        // Act
-        var result = sut.Parse(functionParseResult, context, evaluator, parser);
+            // Act
+            var result = sut.Parse(functionParseResult, context, evaluator, parser);
 
-        // Assert
-        result.Status.Should().Be(ResultStatus.Ok);
-        result.Value.Should().Be("is null");
-    }
+            // Assert
+            result.Status.Should().Be(ResultStatus.Ok);
+            result.Value.Should().Be("is null");
+        }
 
-    [Fact]
-    public void Supplying_Unknown_Context_Type_Gives_Invalid_Result()
-    {
-        // Arrange
-        InitializeParser();
-        var functionParseResult = new FunctionParseResultBuilder()
-            .WithFunctionName("NullCheck")
-            .WithFormatProvider(CultureInfo.InvariantCulture)
-            .Build();
-        var context = this;
-        var evaluator = Fixture.Freeze<IFunctionParseResultEvaluator>();
-        var parser = Fixture.Freeze<IExpressionParser>();
-        var sut = CreateSut();
+        [Fact]
+        public void Returns_Invalid_On_Unsupported_Context_Type()
+        {
+            // Arrange
+            InitializeParser();
+            var functionParseResult = new FunctionParseResultBuilder()
+                .WithFunctionName("NullCheck")
+                .WithFormatProvider(CultureInfo.InvariantCulture)
+                .Build();
+            var context = this;
+            var evaluator = Fixture.Freeze<IFunctionParseResultEvaluator>();
+            var parser = Fixture.Freeze<IExpressionParser>();
+            var sut = CreateSut();
 
-        // Act
-        var result = sut.Parse(functionParseResult, context, evaluator, parser);
+            // Act
+            var result = sut.Parse(functionParseResult, context, evaluator, parser);
 
-        // Assert
-        result.Status.Should().Be(ResultStatus.Invalid);
-        result.ErrorMessage.Should().Be("NullCheck function does not support type ClassFramework.Pipelines.Tests.Shared.Functions.NullCheckFunctionTests, only ContextBase is supported");
-    }
+            // Assert
+            result.Status.Should().Be(ResultStatus.Invalid);
+            result.ErrorMessage.Should().Be("NullCheck function does not support type ClassFramework.Pipelines.Tests.Shared.Functions.NullCheckFunctionTests, only ContextBase is supported");
+        }
 
-    [Fact]
-    public void Supplying_Null_Context_Gives_Invalid_Result()
-    {
-        // Arrange
-        InitializeParser();
-        var functionParseResult = new FunctionParseResultBuilder()
-            .WithFunctionName("NullCheck")
-            .WithFormatProvider(CultureInfo.InvariantCulture)
-            .Build();
-        object? context = null;
-        var evaluator = Fixture.Freeze<IFunctionParseResultEvaluator>();
-        var parser = Fixture.Freeze<IExpressionParser>();
-        var sut = CreateSut();
+        [Fact]
+        public void Returns_Invalid_On_Null_Context()
+        {
+            // Arrange
+            InitializeParser();
+            var functionParseResult = new FunctionParseResultBuilder()
+                .WithFunctionName("NullCheck")
+                .WithFormatProvider(CultureInfo.InvariantCulture)
+                .Build();
+            object? context = null;
+            var evaluator = Fixture.Freeze<IFunctionParseResultEvaluator>();
+            var parser = Fixture.Freeze<IExpressionParser>();
+            var sut = CreateSut();
 
-        // Act
-        var result = sut.Parse(functionParseResult, context, evaluator, parser);
+            // Act
+            var result = sut.Parse(functionParseResult, context, evaluator, parser);
 
-        // Assert
-        result.Status.Should().Be(ResultStatus.Invalid);
-        result.ErrorMessage.Should().Be("NullCheck function does not support type null, only ContextBase is supported");
-    }
+            // Assert
+            result.Status.Should().Be(ResultStatus.Invalid);
+            result.ErrorMessage.Should().Be("NullCheck function does not support type null, only ContextBase is supported");
+        }
 
-    [Fact]
-    public void Sopplying_Wrong_FunctionName_Gives_Continue_Result()
-    {
-        // Arrange
-        InitializeParser();
-        var functionParseResult = new FunctionParseResultBuilder()
-            .WithFunctionName("WrongFunctionName")
-            .WithFormatProvider(CultureInfo.InvariantCulture)
-            .Build();
-        object? context = null;
-        var evaluator = Fixture.Freeze<IFunctionParseResultEvaluator>();
-        var parser = Fixture.Freeze<IExpressionParser>();
-        var sut = CreateSut();
+        [Fact]
+        public void Returns_Continue_On_Wrong_FunctionName()
+        {
+            // Arrange
+            InitializeParser();
+            var functionParseResult = new FunctionParseResultBuilder()
+                .WithFunctionName("WrongFunctionName")
+                .WithFormatProvider(CultureInfo.InvariantCulture)
+                .Build();
+            object? context = null;
+            var evaluator = Fixture.Freeze<IFunctionParseResultEvaluator>();
+            var parser = Fixture.Freeze<IExpressionParser>();
+            var sut = CreateSut();
 
-        // Act
-        var result = sut.Parse(functionParseResult, context, evaluator, parser);
+            // Act
+            var result = sut.Parse(functionParseResult, context, evaluator, parser);
 
-        // Assert
-        result.Status.Should().Be(ResultStatus.Continue);
+            // Assert
+            result.Status.Should().Be(ResultStatus.Continue);
+        }
     }
 }
