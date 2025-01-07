350a4184caf4a6c826df33502f6672e5bef313f7
Paul Deen <paul.deen@dutchworkz.nl>
2025-01-07 07:47:28 +0100

    Added extension method to allow fluent processing of multiple steps with fail-fast

--- src/ClassFramework.Pipelines/Builder/Components/SetNameComponent.cs
+++ src/ClassFramework.Pipelines/Builder/Components/SetNameComponent.cs
@@ -16,22 +16,15 @@ public class SetNameComponent(IFormattableStringParser formattableStringParser) 
     {
         context = context.IsNotNull(nameof(context));
 
-        var results = new ResultDictionaryBuilder<FormattableStringParserResult>()
+        return Task.FromResult(new ResultDictionaryBuilder<FormattableStringParserResult>()
             .Add(NamedResults.Name, () => _formattableStringParser.Parse(context.Request.Settings.BuilderNameFormatString, context.Request.FormatProvider, context.Request))
             .Add(NamedResults.Namespace, () => context.Request.GetMappingMetadata(context.Request.SourceModel.GetFullName()).GetFormattableStringParserResult(MetadataNames.CustomBuilderNamespace, () => _formattableStringParser.Parse(context.Request.Settings.BuilderNamespaceFormatString, context.Request.FormatProvider, context.Request)))
-            .Build();
-
-        var error = results.GetError();
-        if (error is not null)
-        {
-            // Error in formattable string parsing
-            return Task.FromResult<Result>(error);
-        }
-
-        context.Request.Builder
-            .WithName(results[NamedResults.Name].Value!)
-            .WithNamespace(results[NamedResults.Namespace].Value!);
-
-        return Task.FromResult(Result.Continue());
+            .Build()
+            .OnSuccess(results =>
+            {
+                context.Request.Builder
+                    .WithName(results[NamedResults.Name].Value!)
+                    .WithNamespace(results[NamedResults.Namespace].Value!);
+            }));
     }
 }
