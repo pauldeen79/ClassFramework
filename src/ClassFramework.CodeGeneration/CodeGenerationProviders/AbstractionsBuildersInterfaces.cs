6d7123f6dea36d1bf844d1c55e76335ab7af96b8
Paul Deen <paul.deen@dutchworkz.nl>
2025-01-31 21:08:35 +0100

    Fixed builder abstraction interfaces generation when the abstraction is used in multiple inheritance

--- src/ClassFramework.CodeGeneration/CodeGenerationProviders/AbstractionsBuildersInterfaces.cs
+++ src/ClassFramework.CodeGeneration/CodeGenerationProviders/AbstractionsBuildersInterfaces.cs
@@ -6,6 +6,8 @@ public class AbstractionsBuildersInterfaces(IPipelineService pipelineService) : 
     public override Task<Result<IEnumerable<TypeBase>>> GetModel(CancellationToken cancellationToken) => GetBuilderInterfaces(GetAbstractionsInterfaces(), "ClassFramework.Domain.Builders.Abstractions", "ClassFramework.Domain.Abstractions", "ClassFramework.Domain.Builders.Abstractions");
 
     public override string Path => "ClassFramework.Domain/Builders/Abstractions";
-
+    
     protected override bool EnableEntityInheritance => true;
+    //protected override string BuildMethodName => string.Empty; // can't create Build method because interfaces are used on multiple builders (multiple inheritance)
+    protected override string BuildTypedMethodName => string.Empty; // can't create Build method because interfaces are used on multiple builders (multiple inheritance)
 }
