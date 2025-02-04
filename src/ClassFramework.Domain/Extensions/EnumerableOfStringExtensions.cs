6ed14a21b7a1746f3f6062e3f6fae49be4e946bd
Paul Deen <paul.deen@dutchworkz.nl>
2025-02-04 06:57:35 +0100

    Minor: Moved extension method to separate class because it's defined on a different type

---
+++ src/ClassFramework.Domain/Extensions/EnumerableOfStringExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace ClassFramework.Domain.Extensions;
+
+public static class EnumerableOfStringExtensions
+{
+    public static string GetGenericTypeArgumentsString(this IEnumerable<string> instance, bool addBrackets = true)
+    {
+        var prefix = addBrackets ? "<" : string.Empty;
+        var suffix = addBrackets ? ">" : string.Empty;
+
+        var items = instance.ToArray();
+        return items.Length > 0
+            ? $"{prefix}{string.Join(", ", items)}{suffix}"
+            : string.Empty;
+    }
+}
--- src/ClassFramework.Domain/Extensions/GenericTypeArgumentsContainerExtensions.cs
+++ src/ClassFramework.Domain/Extensions/GenericTypeArgumentsContainerExtensions.cs
@@ -5,17 +5,6 @@ public static class GenericTypeArgumentsContainerExtensions
     public static string GetGenericTypeArgumentsString(this IGenericTypeArgumentsContainer instance, bool addBrackets = true)
         => instance.GenericTypeArguments.GetGenericTypeArgumentsString(addBrackets);
 
-    public static string GetGenericTypeArgumentsString(this IEnumerable<string> instance, bool addBrackets = true)
-    {
-        var prefix = addBrackets ? "<" : string.Empty;
-        var suffix = addBrackets ? ">" : string.Empty;
-
-        var items = instance.ToArray();
-        return items.Length > 0
-            ? $"{prefix}{string.Join(", ", items)}{suffix}"
-            : string.Empty;
-    }
-
     public static string GetGenericTypeArgumentConstraintsString(this IGenericTypeArgumentsContainer instance, int indent)
         => instance.GenericTypeArgumentConstraints.Count > 0
             ? string.Concat(Environment.NewLine,
