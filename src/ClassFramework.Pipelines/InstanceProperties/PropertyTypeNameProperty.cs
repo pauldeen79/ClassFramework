33827afff1bb242d8ef218036f226b77a41442da
Paul Deen <paul.deen@dutchworkz.nl>
2025-06-01 15:01:13 +0200

    Fixed typename mapping

--- src/ClassFramework.Pipelines/InstanceProperties/PropertyTypeNameProperty.cs
+++ src/ClassFramework.Pipelines/InstanceProperties/PropertyTypeNameProperty.cs
@@ -5,12 +5,15 @@
 [MemberResultType(typeof(string))]
 public class PropertyTypeNameProperty : IProperty
 {
-    public Task<Result<object?>> EvaluateAsync(FunctionCallContext context, CancellationToken token)
-        => Task.Run(() =>
-        {
-            context = ArgumentGuard.IsNotNull(context, nameof(context));
+    public async Task<Result<object?>> EvaluateAsync(FunctionCallContext context, CancellationToken token)
+    {
+        context = ArgumentGuard.IsNotNull(context, nameof(context));
 
-            return context.GetInstanceValueResult<Property>()
-                .Transform<object?>(property => property.TypeName);
-        }, token);
+        return (await new AsyncResultDictionaryBuilder()
+            .Add("instance", context.GetInstanceValueResult<Property>())
+            .Add("mappedContextBase", context.GetMappedContextBaseAsync())
+            .Build()
+            .ConfigureAwait(false))
+            .OnSuccess(results => results.GetValue<MappedContextBase>("mappedContextBase").MapTypeName(results.GetValue<Property>("instance").TypeName));
+    }
 }
