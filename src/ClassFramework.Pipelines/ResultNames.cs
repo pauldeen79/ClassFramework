d5535d74fe8320c6b215c0b14a3fa890b5dc54b9
Paul Deen <paul.deen@dutchworkz.nl>
2025-06-06 19:23:27 +0200

    Attempt to fix code coverage issue

--- src/ClassFramework.Pipelines/ResultNames.cs
+++ src/ClassFramework.Pipelines/ResultNames.cs
@@ -5,8 +5,8 @@ public static class ResultNames
     public const string Class = "class";
     public const string CollectionTypeName = "collectionTypeName";
     public const string AddMethodNameFormatString = "addMethodNameFormatString";
-    public const string Settings = nameof(Settings);
-    public const string TypeName = nameof(TypeName);
+    public const string Settings = "Settings";
+    public const string TypeName = "TypeName";
     public const string Context = "context";
     public const string Property = "property";
 }
