c026f09429cad1967e2fced3ca5458a52cef0b65
Paul Deen <paul.deen@dutchworkz.nl>
2025-07-19 15:18:01 +0200

    Fixed TODOs

--- src/ClassFramework.Pipelines/InstanceProperties/PropertyBuilderFuncPrefixProperty.cs
+++ src/ClassFramework.Pipelines/InstanceProperties/PropertyBuilderFuncPrefixProperty.cs
@@ -10,13 +10,11 @@ public class PropertyBuilderFuncPrefixProperty : IProperty
         context = ArgumentGuard.IsNotNull(context, nameof(context));
 
         return await context.EvaluateForProperty(
-            (property, settings) =>
+            (property, settings, mappedContextBase) =>
             {
-                //TODO: Check if we can get access to mapping medatadata here...
-                //var metadata = context.Request.GetMappingMetadata(property.TypeName).ToArray();
-                //var builderName = metadata.GetStringValue(MetadataNames.CustomBuilderName, DefaultBuilderName);
-                //var useBuilderLazyValues = settings.UseBuilderLazyValues && builderName == DefaultBuilderName;
-                var useBuilderLazyValues = settings.UseBuilderLazyValues;
+                var metadata = mappedContextBase.GetMappingMetadata(property.TypeName).ToArray();
+                var builderName = metadata.GetStringValue(MetadataNames.CustomBuilderName, ContextBase.DefaultBuilderName);
+                var useBuilderLazyValues = settings.UseBuilderLazyValues && builderName == ContextBase.DefaultBuilderName;
                 return useBuilderLazyValues
                     ? $"new {property.TypeName.ToLazy()}(() => "
                     : string.Empty;
