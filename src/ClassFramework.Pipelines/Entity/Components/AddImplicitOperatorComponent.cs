612930b770a3b3971a1b002779d1c5a3d13b2812
Paul Deen <paul.deen@dutchworkz.nl>
2025-08-10 08:30:08 +0200

    Trick Sonar, part 2

--- src/ClassFramework.Pipelines/Entity/Components/AddImplicitOperatorComponent.cs
+++ src/ClassFramework.Pipelines/Entity/Components/AddImplicitOperatorComponent.cs
@@ -32,14 +32,19 @@ public class AddImplicitOperatorComponent(IExpressionEvaluator evaluator) : IPip
 
         var name = results.GetValue(ResultNames.Name).ToString();
         var ns = results.GetValue(ResultNames.Namespace).ToString();
-        var entityFullName = context.Request.GetEntityFullName(ns, name);
         var entityConcreteFullName = context.Request.GetEntityConcreteFullName(ns, name);
+        var entityFullName = context.Request.GetEntityFullName(ns, name);
         var metadata = context.Request.GetMappingMetadata(entityFullName).ToArray();
         var customNamespaceResults = new ResultDictionaryBuilder<string>()
             .Add("CustomBuilderNamespace", () => metadata.GetStringResult(MetadataNames.CustomBuilderNamespace, () => Result.Success($"{ns.AppendWhenNotNullOrEmpty(".")}Builders")))
             .Add("CustomBuilderInterfaceNamespace", () => metadata.GetStringResult(MetadataNames.CustomBuilderInterfaceNamespace, () => Result.Success(context.Request.GetBuilderInterfaceNamespace(results.GetValue("BuilderInterfaceNamespace").ToString(), ns))))
             .Add("CustomConcreteBuilderNamespace", () => context.Request.GetMappingMetadata(entityConcreteFullName).GetStringResult(MetadataNames.CustomBuilderNamespace, () => Result.Success($"{ns.AppendWhenNotNullOrEmpty(".")}Builders")))
             .Build();
+
+        var builderConcreteName = context.Request.Settings.EnableInheritance
+            && context.Request.Settings.BaseClass is null
+                ? name
+                : name.ReplaceSuffix("Base", string.Empty, StringComparison.Ordinal);
 
         var customNamespaceError = customNamespaceResults.GetError();
         if (customNamespaceError is not null)
@@ -48,13 +53,8 @@ public class AddImplicitOperatorComponent(IExpressionEvaluator evaluator) : IPip
             return customNamespaceError;
         }
 
-        var builderConcreteName = context.Request.Settings.EnableInheritance
-            && context.Request.Settings.BaseClass is null
-                ? name
-                : name.ReplaceSuffix("Base", string.Empty, StringComparison.Ordinal);
-
-        var builderName = results.GetValue(ResultNames.BuilderName).ToString().Replace(context.Request.SourceModel.Name, builderConcreteName);
         var generics = context.Request.SourceModel.GetGenericTypeArgumentsString();
+        var builderName = results.GetValue(ResultNames.BuilderName).ToString().Replace(context.Request.SourceModel.Name, builderConcreteName);
         var builderConcreteTypeName = $"{customNamespaceResults.GetValue("CustomBuilderNamespace")}.{builderName}";
         var builderTypeName = context.Request.GetBuilderTypeName(customNamespaceResults.GetValue("CustomBuilderInterfaceNamespace"), customNamespaceResults.GetValue("CustomConcreteBuilderNamespace"), builderConcreteName, builderConcreteTypeName, results.GetValue(ResultNames.BuilderName));
 
