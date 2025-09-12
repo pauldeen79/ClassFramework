711462e0cff78c5ee35aa569bde9a3392bd7bd38
Paul Deen <paul.deen@dutchworkz.nl>
2025-09-12 17:17:27 +0200

    Fixed TODO, this has been implemented in CrossCutting in the meantime

--- src/ClassFramework.Pipelines/Extensions/AsyncResultDictionaryExtensions.cs
+++ src/ClassFramework.Pipelines/Extensions/AsyncResultDictionaryExtensions.cs
@@ -3,6 +3,5 @@
 public static class AsyncResultDictionaryExtensions
 {
     public static bool NeedNonLazyOverloads(this IReadOnlyDictionary<string, Result<GenericFormattableString>> instance)
-        //TODO: Add functionality to GenericFormattableString, so we can compare them by value (just like string)
-        => instance.GetValue(ResultNames.TypeName).ToString() != instance.GetValue(ResultNames.NonLazyTypeName).ToString();
+        => instance.GetValue(ResultNames.TypeName) != instance.GetValue(ResultNames.NonLazyTypeName);
 }
