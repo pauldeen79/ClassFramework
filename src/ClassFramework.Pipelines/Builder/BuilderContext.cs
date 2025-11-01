c07ef6d2e2d284f952c12c03dc334e0132618261
Paul Deen <paul.deen@dutchworkz.nl>
2025-11-01 10:19:48 +0100

    Refactored part of ContextExtensions class, which was speciffic for BuilderContext

--- src/ClassFramework.Pipelines/Builder/BuilderContext.cs
+++ src/ClassFramework.Pipelines/Builder/BuilderContext.cs
@@ -155,6 +155,47 @@ public class BuilderContext(TypeBase sourceModel, PipelineSettings settings, IFo
             );
     }
 
+    public async Task<Result<GenericFormattableString>> CreateEntityInstanciationAsync(
+        IExpressionEvaluator evaluator,
+        ICsharpExpressionDumper csharpExpressionDumper,
+        string classNameSuffix,
+        CancellationToken token)
+    {
+        evaluator = evaluator.IsNotNull(nameof(evaluator));
+
+        var customEntityInstanciation = GetMappingMetadata(SourceModel.GetFullName())
+            .GetStringValue(MetadataNames.CustomBuilderEntityInstanciation);
+        if (!string.IsNullOrEmpty(customEntityInstanciation))
+        {
+            return await evaluator.EvaluateInterpolatedStringAsync(customEntityInstanciation, FormatProvider, this, token).ConfigureAwait(false);
+        }
+
+        if (SourceModel is not IConstructorsContainer constructorsContainer)
+        {
+            return Result.Invalid<GenericFormattableString>("Cannot create an instance of a type that does not have constructors");
+        }
+
+        if (SourceModel is Class cls && cls.Abstract)
+        {
+            return Result.Invalid<GenericFormattableString>("Cannot create an instance of an abstract class");
+        }
+
+        var hasPublicParameterlessConstructor = constructorsContainer.HasPublicParameterlessConstructor();
+        var openSign = GetBuilderPocoOpenSign(hasPublicParameterlessConstructor && SourceModel.Properties.Count != 0);
+        var closeSign = GetBuilderPocoCloseSign(hasPublicParameterlessConstructor && SourceModel.Properties.Count != 0);
+
+        var parametersResult = await GetConstructionMethodParametersAsync(evaluator, csharpExpressionDumper, hasPublicParameterlessConstructor, token).ConfigureAwait(false);
+        if (!parametersResult.IsSuccessful())
+        {
+            return parametersResult;
+        }
+
+        var entityNamespace = GetMappingMetadata(SourceModel.GetFullName()).GetStringValue(MetadataNames.CustomEntityNamespace, () => SourceModel.Namespace);
+        var ns = MapNamespace(entityNamespace).AppendWhenNotNullOrEmpty(".");
+
+        return Result.Success<GenericFormattableString>($"new {ns}{SourceModel.Name}{classNameSuffix}{SourceModel.GetGenericTypeArgumentsString()}{openSign}{parametersResult.Value}{closeSign}");
+    }
+
     public ClassBuilder Builder { get; } = new();
 
     private string ReturnValue
@@ -187,6 +228,123 @@ public class BuilderContext(TypeBase sourceModel, PipelineSettings settings, IFo
             return typeof(IBuilder<object>).ReplaceGenericTypeName(BuildReturnTypeName);
         }
     }
+
+    private async Task<Result<GenericFormattableString>> GetConstructionMethodParametersAsync(IExpressionEvaluator evaluator, ICsharpExpressionDumper csharpExpressionDumper, bool hasPublicParameterlessConstructor, CancellationToken token)
+    {
+        var properties = SourceModel.GetBuilderConstructorProperties(this);
+
+        var results = new List<ConstructionMethodParameterInfo>();
+        foreach (var property in properties)
+        {
+            var useBuilderLazyValues = UseBuilderLazyValues(property.TypeName);
+            var info =
+                new ConstructionMethodParameterInfo(
+                    property.Name,
+                    property,
+                    await evaluator.EvaluateInterpolatedStringAsync
+                    (
+                        GetMappingMetadata(property.TypeName).GetStringValue(MetadataNames.CustomBuilderMethodParameterExpression, PlaceholderNames.NamePlaceholder),
+                        FormatProvider,
+                        new ParentChildContext<BuilderContext, Property>(this, property, this.Settings),
+                        token
+                    ).ConfigureAwait(false),
+                    GetMappingMetadata
+                    (
+                        property.TypeName.FixTypeName().WithoutGenerics() // i.e. List<> etc.
+                    ).GetStringValue(MetadataNames.CustomCollectionInitialization, () => "[Expression]"),
+                    property.GetSuffix(Settings.EnableNullableReferenceTypes, csharpExpressionDumper, this),
+                    useBuilderLazyValues
+                );
+
+            results.Add(info);
+
+            if (!info.Result.IsSuccessful())
+            {
+                break;
+            }
+        }
+
+        var error = results.Find(x => !x.Result.IsSuccessful());
+        if (error is not null)
+        {
+            return error.Result;
+        }
+
+        return Result.Success<GenericFormattableString>(string.Join(", ", results.Select(x => $"{GetPrefix(hasPublicParameterlessConstructor, x.Name)}{GetBuilderPropertyExpression(x.Result.Value!, x.Source, x.CollectionInitializer, x.Suffix, x.UseBuilderLazyValues)}")));
+    }
+
+    private static string GetPrefix(bool hasPublicParameterlessConstructor, string propertyName)
+        => hasPublicParameterlessConstructor
+            ? $"{propertyName} = "
+            : string.Empty;
+
+    private static string? GetBuilderPropertyExpression(string? value, Property sourceProperty, string collectionInitializer, string suffix, bool useBuilderLazyValues)
+    {
+        if (value is null || !value.Contains(PlaceholderNames.NamePlaceholder))
+        {
+            return value;
+        }
+
+        var lazySuffix = GetLazySuffix(sourceProperty, useBuilderLazyValues);
+
+        if (value == PlaceholderNames.NamePlaceholder)
+        {
+            return sourceProperty.Name + lazySuffix;
+        }
+
+        if (sourceProperty.TypeName.FixTypeName().IsCollectionTypeName())
+        {
+            var valueExpression = value!
+                .Replace(PlaceholderNames.NamePlaceholder, "x")
+                .Replace("[NullableSuffix]", string.Empty)
+                .Replace("[ForcedNullableSuffix]", sourceProperty.IsValueType
+                    ? string.Empty
+                    : "!");
+
+            return collectionInitializer
+                .Replace("[Type]", sourceProperty.TypeName.FixTypeName().WithoutGenerics())
+                .Replace("[Generics]", sourceProperty.TypeName.FixTypeName().GetGenericArguments(addBrackets: true))
+                .Replace("[Expression]", $"{sourceProperty.Name}{suffix}.Select(x => {valueExpression}{lazySuffix})");
+        }
+        else
+        {
+            var valueExpression = value!
+                .Replace(PlaceholderNames.NamePlaceholder, sourceProperty.Name)
+                .Replace("[NullableSuffix]", suffix)
+                .Replace("[ForcedNullableSuffix]", string.IsNullOrEmpty(suffix)
+                    ? string.Empty
+                    : "!");
+
+            return $"{valueExpression}{lazySuffix}";
+        }
+    }
+
+    private static string GetLazySuffix(Property sourceProperty, bool useBuilderLazyValues)
+    {
+        if (!useBuilderLazyValues)
+        {
+            return string.Empty;
+        }
+
+        if (sourceProperty.TypeName.FixTypeName().IsCollectionTypeName())
+        {
+            // for a collection property, we need to call Invoke using () on each item
+            return ".Select(x => x())";
+        }
+
+        // for a non collection property, we can just call the Invoke using ()
+        return "()";
+    }
+
+    private static string GetBuilderPocoCloseSign(bool poco)
+        => poco
+            ? " }"
+            : ")";
+
+    private static string GetBuilderPocoOpenSign(bool poco)
+        => poco
+            ? " { "
+            : "(";
 
     public override object GetResponseBuilder() => Builder;
 
