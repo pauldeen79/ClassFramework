5353cfba6599b769b5f2478b4b6b1000206bb1ae
Paul Deen <paul.deen@dutchworkz.nl>
2025-11-01 10:23:44 +0100

    Moved Builder specific class into Builder namespace

--- src/ClassFramework.Pipelines/Extensions/ContextExtensions.cs
+++ src/ClassFramework.Pipelines/Extensions/ContextExtensions.cs
@@ -41,23 +41,3 @@ public static class ContextExtensions
         return Result.Success();
     }
 }
-
-internal sealed class ConstructionMethodParameterInfo
-{
-    public string Name { get; }
-    public Property Source { get; }
-    public Result<GenericFormattableString> Result { get; }
-    public string CollectionInitializer { get; }
-    public string Suffix { get; }
-    public bool UseBuilderLazyValues { get; }
-
-    public ConstructionMethodParameterInfo(string name, Property source, Result<GenericFormattableString> result, string collectionInitializer, string suffix, bool useBuilderLazyValues)
-    {
-        Name = name;
-        Source = source;
-        Result = result;
-        CollectionInitializer = collectionInitializer;
-        Suffix = suffix;
-        UseBuilderLazyValues = useBuilderLazyValues;
-    }
-}
