562bbdcfc09cf1e22d3058da33ccf509b9ada40a
Paul Deen <paul.deen@dutchworkz.nl>
2025-11-08 20:57:57 +0100

    Removed unused code after refactoring

--- src/ClassFramework.Pipelines/Reflection/ReflectionContext.cs
+++ src/ClassFramework.Pipelines/Reflection/ReflectionContext.cs
@@ -2,12 +2,9 @@
 
 public class ReflectionContext : ContextBase<Type>
 {
-    private readonly TypeBaseBuilderWrapper _wrappedBuilder;
-
     public ReflectionContext(Type sourceModel, PipelineSettings settings, IFormatProvider formatProvider, CancellationToken cancellationToken)
         : base(sourceModel, settings, formatProvider, cancellationToken)
     {
-        _wrappedBuilder = new TypeBaseBuilderWrapper(SourceModel);
     }
 
     protected override string NewCollectionTypeName => Settings.EntityNewCollectionTypeName;
